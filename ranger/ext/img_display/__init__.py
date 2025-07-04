63eb0da757a0151a4ceb122488f6bb7b46be6235
Ethan Milon <ethan@ethanmilon.dev>
2025-07-04 22:20:21 +0200

    img_display: extract sixel displayer

--- ranger/ext/img_display/__init__.py
+++ ranger/ext/img_display/__init__.py
@@ -14,14 +14,13 @@ from .displayer import (
     get_image_displayer,
     temporarily_moved_cursor,
     move_cur,
-    get_font_dimensions,
     ImageDisplayer,
     ImageDisplayError,
     ImgDisplayUnsupportedException,
     register_image_displayer,
 )
 
-from . import iterm, w3m  # noqa: F401
+from . import sixel, iterm, w3m  # noqa: F401
 
 __all__ = [
     "get_image_displayer",
@@ -37,18 +36,14 @@ import struct
 import sys
 import warnings
 import json
-import mmap
 import threading
-from subprocess import Popen, PIPE, check_call, CalledProcessError
-from collections import namedtuple
+from subprocess import Popen, PIPE 
 
 import termios
 import codecs
-from tempfile import gettempdir, NamedTemporaryFile, TemporaryFile
+from tempfile import gettempdir, NamedTemporaryFile
 
-from ranger import PY3
 from ranger.core.shared import FileManagerAware
-from ranger.ext.popen23 import DEVNULL
 from ranger.ext.which import which
 
 
@@ -58,101 +53,6 @@ if which("magick"):
 else:
     # Magick < 7
     MAGICK_CONVERT_CMD_BASE = ("convert",)
-
-_CacheableSixelImage = namedtuple("_CacheableSixelImage", ("width", "height", "inode"))
-
-_CachedSixelImage = namedtuple("_CachedSixelImage", ("image", "fh"))
-
-
-@register_image_displayer("sixel")
-class SixelImageDisplayer(ImageDisplayer, FileManagerAware):
-    """Implementation of ImageDisplayer using SIXEL."""
-
-    def __init__(self):
-        self.win = None
-        self.cache = {}
-        self.fm.signal_bind('preview.cleared', lambda signal: self._clear_cache(signal.path))
-
-    def _clear_cache(self, path):
-        if os.path.exists(path):
-            self.cache = {
-                ce: cd
-                for ce, cd in self.cache.items()
-                if ce.inode != os.stat(path).st_ino
-            }
-
-    def _sixel_cache(self, path, width, height):
-        stat = os.stat(path)
-        cacheable = _CacheableSixelImage(width, height, stat.st_ino)
-
-        if cacheable not in self.cache:
-            font_width, font_height = get_font_dimensions()
-            fit_width = font_width * width
-            fit_height = font_height * height
-
-            sixel_dithering = self.fm.settings.sixel_dithering
-            cached = TemporaryFile("w+", prefix="ranger", suffix=path.replace(os.sep, "-"))
-
-            environ = dict(os.environ)
-            environ.setdefault("MAGICK_OCL_DEVICE", "true")
-            try:
-                check_call(
-                    [
-                        *MAGICK_CONVERT_CMD_BASE,
-                        path + "[0]",
-                        "-geometry",
-                        "{0}x{1}>".format(fit_width, fit_height),
-                        "-dither",
-                        sixel_dithering,
-                        "sixel:-",
-                    ],
-                    stdout=cached,
-                    stderr=DEVNULL,
-                    env=environ,
-                )
-            except CalledProcessError:
-                raise ImageDisplayError("ImageMagick failed processing the SIXEL image")
-            except FileNotFoundError:
-                raise ImageDisplayError("SIXEL image previews require ImageMagick")
-            finally:
-                cached.flush()
-
-            if os.fstat(cached.fileno()).st_size == 0:
-                raise ImageDisplayError("ImageMagick produced an empty SIXEL image file")
-
-            self.cache[cacheable] = _CachedSixelImage(mmap.mmap(cached.fileno(), 0), cached)
-
-        return self.cache[cacheable].image
-
-    # pylint: disable=too-many-positional-arguments
-    def draw(self, path, start_x, start_y, width, height):
-        if self.win is None:
-            self.win = self.fm.ui.win.subwin(height, width, start_y, start_x)
-        else:
-            self.win.mvwin(start_y, start_x)
-            self.win.resize(height, width)
-
-        with temporarily_moved_cursor(start_y, start_x):
-            sixel = self._sixel_cache(path, width, height)[:]
-            if PY3:
-                sys.stdout.buffer.write(sixel)
-            else:
-                sys.stdout.write(sixel)
-            sys.stdout.flush()
-
-    def clear(self, start_x, start_y, width, height):
-        if self.win is not None:
-            self.win.clear()
-            self.win.refresh()
-
-            self.win = None
-
-        self.fm.ui.win.redrawwin()
-
-    def quit(self):
-        self.clear(0, 0, 0, 0)
-        self.cache = {}
-
 
 @register_image_displayer("terminology")
 class TerminologyImageDisplayer(ImageDisplayer, FileManagerAware):
