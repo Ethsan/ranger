83695c03c7fba7c459d456e43411ae7e2b6a43c4
Ethan Milon <ethan@ethanmilon.dev>
2025-07-04 22:22:18 +0200

    img_display: extract terminology displayer

--- ranger/ext/img_display/__init__.py
+++ ranger/ext/img_display/__init__.py
@@ -13,14 +13,13 @@ from __future__ import (absolute_import, division, print_function)
 from .displayer import (
     get_image_displayer,
     temporarily_moved_cursor,
-    move_cur,
     ImageDisplayer,
     ImageDisplayError,
     ImgDisplayUnsupportedException,
     register_image_displayer,
 )
 
-from . import sixel, iterm, w3m  # noqa: F401
+from . import terminology, sixel, iterm, w3m  # noqa: F401
 
 __all__ = [
     "get_image_displayer",
@@ -53,46 +52,6 @@ if which("magick"):
 else:
     # Magick < 7
     MAGICK_CONVERT_CMD_BASE = ("convert",)
-
-@register_image_displayer("terminology")
-class TerminologyImageDisplayer(ImageDisplayer, FileManagerAware):
-    """Implementation of ImageDisplayer using terminology image display support
-    (https://github.com/billiob/terminology).
-
-    Ranger must be running in terminology for this to work.
-    Doesn't work with TMUX :/
-    """
-
-    def __init__(self):
-        self.display_protocol = "\033"
-        self.close_protocol = "\000"
-
-    # pylint: disable=too-many-positional-arguments
-    def draw(self, path, start_x, start_y, width, height):
-        with temporarily_moved_cursor(start_y, start_x):
-            # Write intent
-            sys.stdout.write("%s}ic#%d;%d;%s%s" % (
-                self.display_protocol,
-                width, height,
-                path,
-                self.close_protocol))
-
-            # Write Replacement commands ('#')
-            for y in range(0, height):
-                move_cur(start_y + y, start_x)
-                sys.stdout.write("%s}ib%s%s%s}ie%s\n" % (  # needs a newline to work
-                    self.display_protocol,
-                    self.close_protocol,
-                    "#" * width,
-                    self.display_protocol,
-                    self.close_protocol))
-
-    def clear(self, start_x, start_y, width, height):
-        self.fm.ui.win.redrawwin()
-        self.fm.ui.win.refresh()
-
-    def quit(self):
-        self.clear(0, 0, 0, 0)
 
 
 @register_image_displayer("urxvt")
