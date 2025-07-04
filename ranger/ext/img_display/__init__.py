1f9521940ee88558998c0adb804da9e68703a0d7
Ethan Milon <ethan@ethanmilon.dev>
2025-07-04 22:10:04 +0200

    img_display: extract w3m displayer

--- ranger/ext/img_display/__init__.py
+++ ranger/ext/img_display/__init__.py
@@ -22,6 +22,8 @@ from .displayer import (
     register_image_displayer,
 )
 
+from . import w3m  # noqa: F401
+
 __all__ = [
     "get_image_displayer",
     "ImageDisplayer",
@@ -30,8 +32,6 @@ __all__ = [
 ]
 
 import base64
-import curses
-import errno
 import fcntl
 import os
 import struct
@@ -41,16 +41,15 @@ import json
 import mmap
 import threading
 from subprocess import Popen, PIPE, check_call, CalledProcessError
-from collections import defaultdict, namedtuple
+from collections import namedtuple
 
 import termios
-from contextlib import contextmanager
 import codecs
 from tempfile import gettempdir, NamedTemporaryFile, TemporaryFile
 
 from ranger import PY3
-from ranger.core.shared import FileManagerAware, SettingsAware
-from ranger.ext.popen23 import Popen23, DEVNULL
+from ranger.core.shared import FileManagerAware
+from ranger.ext.popen23 import DEVNULL
 from ranger.ext.which import which
 
 
@@ -61,174 +60,6 @@ else:
     # Magick < 7
     MAGICK_CONVERT_CMD_BASE = ("convert",)
 
-
-W3MIMGDISPLAY_ENV = "W3MIMGDISPLAY_PATH"
-W3MIMGDISPLAY_OPTIONS = []
-W3MIMGDISPLAY_PATHS = [
-    '/usr/lib/w3m/w3mimgdisplay',
-    '/usr/libexec/w3m/w3mimgdisplay',
-    '/usr/lib64/w3m/w3mimgdisplay',
-    '/usr/libexec64/w3m/w3mimgdisplay',
-    '/usr/local/libexec/w3m/w3mimgdisplay',
-]
-
-@register_image_displayer("w3m")
-class W3MImageDisplayer(ImageDisplayer, FileManagerAware):
-    """Implementation of ImageDisplayer using w3mimgdisplay, an utilitary
-    program from w3m (a text-based web browser). w3mimgdisplay can display
-    images either in virtual tty (using linux framebuffer) or in a Xorg session.
-    Does not work over ssh.
-
-    w3m need to be installed for this to work.
-    """
-    is_initialized = False
-
-    def __init__(self):
-        self.binary_path = None
-        self.process = None
-
-    def initialize(self):
-        """start w3mimgdisplay"""
-        self.binary_path = None
-        self.binary_path = self._find_w3mimgdisplay_executable()  # may crash
-        # We cannot close the process because that stops the preview.
-        # pylint: disable=consider-using-with
-        self.process = Popen([self.binary_path] + W3MIMGDISPLAY_OPTIONS, cwd=self.working_dir,
-                             stdin=PIPE, stdout=PIPE, universal_newlines=True)
-        self.is_initialized = True
-
-    @staticmethod
-    def _find_w3mimgdisplay_executable():
-        paths = [os.environ.get(W3MIMGDISPLAY_ENV, None)] + W3MIMGDISPLAY_PATHS
-        for path in paths:
-            if path is not None and os.path.exists(path):
-                return path
-        raise ImageDisplayError("No w3mimgdisplay executable found.  Please set "
-                                "the path manually by setting the %s environment variable.  (see "
-                                "man page)" % W3MIMGDISPLAY_ENV)
-
-    def _get_font_dimensions(self):
-        # Get the height and width of a character displayed in the terminal in
-        # pixels.
-        if self.binary_path is None:
-            self.binary_path = self._find_w3mimgdisplay_executable()
-        farg = struct.pack("HHHH", 0, 0, 0, 0)
-        fd_stdout = sys.stdout.fileno()
-        fretint = fcntl.ioctl(fd_stdout, termios.TIOCGWINSZ, farg)
-        rows, cols, xpixels, ypixels = struct.unpack("HHHH", fretint)
-        if xpixels == 0 and ypixels == 0:
-            with Popen23(
-                [self.binary_path, "-test"],
-                stdout=PIPE,
-                universal_newlines=True,
-            ) as process:
-                output, _ = process.communicate()
-            output = output.split()
-            xpixels, ypixels = int(output[0]), int(output[1])
-            # adjust for misplacement
-            xpixels += 2
-            ypixels += 2
-
-        return (xpixels // cols), (ypixels // rows)
-
-    # pylint: disable=too-many-positional-arguments
-    def draw(self, path, start_x, start_y, width, height):
-        if not self.is_initialized or self.process.poll() is not None:
-            self.initialize()
-        input_gen = self._generate_w3m_input(path, start_x, start_y, width,
-                                             height)
-        self.process.stdin.write(input_gen)
-        self.process.stdin.flush()
-        self.process.stdout.readline()
-
-        # Mitigate the issue with the horizontal black bars when
-        # selecting some images on some systems. 2 milliseconds seems
-        # enough. Adjust as necessary.
-        if self.fm.settings.w3m_delay > 0:
-            from time import sleep
-            sleep(self.fm.settings.w3m_delay)
-
-        # HACK workaround for w3mimgdisplay memory leak
-        self.quit()
-        self.is_initialized = False
-
-    def clear(self, start_x, start_y, width, height):
-        if not self.is_initialized or self.process.poll() is not None:
-            self.initialize()
-
-        fontw, fonth = self._get_font_dimensions()
-
-        cmd = "6;{x};{y};{w};{h}\n4;\n3;\n".format(
-            x=int((start_x - 0.2) * fontw),
-            y=start_y * fonth,
-            # y = int((start_y + 1) * fonth), # (for tmux top status bar)
-            w=int((width + 0.4) * fontw),
-            h=height * fonth + 1,
-            # h = (height - 1) * fonth + 1, # (for tmux top status bar)
-        )
-
-        try:
-            self.fm.ui.win.redrawwin()
-            self.process.stdin.write(cmd)
-        except IOError as ex:
-            if ex.errno == errno.EPIPE:
-                return
-            raise
-        self.process.stdin.flush()
-        self.process.stdout.readline()
-
-    # pylint: disable=too-many-positional-arguments
-    def _generate_w3m_input(self, path, start_x, start_y, max_width, max_height):
-        """Prepare the input string for w3mimgpreview
-
-        start_x, start_y, max_height and max_width specify the drawing area.
-        They are expressed in number of characters.
-        """
-        fontw, fonth = self._get_font_dimensions()
-        if fontw == 0 or fonth == 0:
-            raise ImgDisplayUnsupportedException
-
-        max_width_pixels = max_width * fontw
-        max_height_pixels = max_height * fonth - 2
-        # (for tmux top status bar)
-        # max_height_pixels = (max_height - 1) * fonth - 2
-
-        # get image size
-        cmd = "5;{path}\n".format(path=path)
-
-        self.process.stdin.write(cmd)
-        self.process.stdin.flush()
-        output = self.process.stdout.readline().split()
-
-        if len(output) != 2:
-            raise ImageDisplayError('Failed to execute w3mimgdisplay', output)
-
-        width = int(output[0])
-        height = int(output[1])
-
-        # get the maximum image size preserving ratio
-        if width > max_width_pixels:
-            height = (height * max_width_pixels) // width
-            width = max_width_pixels
-        if height > max_height_pixels:
-            width = (width * max_height_pixels) // height
-            height = max_height_pixels
-
-        start_x = int((start_x - 0.2) * fontw) + self.fm.settings.w3m_offset
-        start_y = (start_y * fonth) + self.fm.settings.w3m_offset
-
-        return "0;1;{x};{y};{w};{h};;;;;{filename}\n4;\n3;\n".format(
-            x=start_x,
-            y=start_y,
-            # y = (start_y + 1) * fonth, # (for tmux top status bar)
-            w=width,
-            h=height,
-            filename=path,
-        )
-
-    def quit(self):
-        if self.is_initialized and self.process and self.process.poll() is None:
-            self.process.kill()
 
 # TODO: remove FileManagerAwareness, as stuff in ranger.ext should be
 # ranger-independent libraries.
